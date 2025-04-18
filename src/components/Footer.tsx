9b8f3790a1b4485f362bc305629af02d1a4a7442
adewarainioluwa@gmail.com <adewara@gmail.com>
2025-04-18 22:34:27 +0100

    fix academy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="w-full py-10 bg-[#021343] ">
       <div className="container mx-auto ">
         <p className="text-white w-[80%] md:w-full text-center">
-          Copyright <span className="text-2xl">©</span> Grace Destiny Academ.
+          Copyright <span className="text-2xl">©</span> Grace Destiny Academy.
           CAC Registration Number - RC2438266 . All rights reserved.
         </p>
       </div>
